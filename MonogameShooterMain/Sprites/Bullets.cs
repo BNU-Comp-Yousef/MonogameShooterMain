24d00e446cc3aec8c798c544f5de653e6329963e
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 21:56:29 +0100

    added temp code to bullets sprite

--- MonogameShooterMain/Sprites/Bullets.cs
+++ MonogameShooterMain/Sprites/Bullets.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace MonogameShooterMain.Sprites
 {
-    class Bullets
+    public class Bullets : Sprite, CollidableI
     {
+        private float _timer;
+
+        public float LifeSpan { get; set; }
+
+        public Vector2 Velocity { get; set; }
+
+        public Bullets(Texture2D texture) : base(texture)
+        {
+
+        }
+
+        public override void Update(GameTime gt)
+        {
+            _timer += (float)gt.ElapsedGameTime.TotalSeconds;
+
+            if (_timer >= LifeSpan)
+                IsRemoved = true;
+            Position += Velocity;
+        }
+
+        public void CollideOn(Sprite sp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
