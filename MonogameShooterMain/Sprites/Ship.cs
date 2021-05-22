e86f565abfd3387a184092253c98842cf87f0b57
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 22:07:23 +0100

    added code temp to ship to initiate base movement and commands

--- MonogameShooterMain/Sprites/Ship.cs
+++ MonogameShooterMain/Sprites/Ship.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace MonogameShooterMain.Sprites
 {
-    class Ship
+    public class Ship : Sprite, CollidableI
     {
+        public int Health { get; set; }
+
+        public Bullets Bullet { get; set; }
+
+        public float Speed;
+
+        public Ship(Texture2D texture) : base(texture)
+        {
+        }
+
+        protected void Shoot(float speed)
+        {
+            //You cant shoot if you haven't added a bullet prefab.
+            if (Bullet == null)
+                return;
+
+            var bullet = Bullet.Clone() as Bullets;
+            bullet.Position = this.Position;
+            bullet.Colour = this.Colour;
+            bullet.Layer = 0.1f;
+            bullet.LifeSpan = 5f;
+            bullet.Velocity = new Vector2(speed, 0f);
+            bullet.Parent = this;
+
+            Children.Add(bullet);
+        }
+
+        public virtual void CollideOn(Sprite sp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
