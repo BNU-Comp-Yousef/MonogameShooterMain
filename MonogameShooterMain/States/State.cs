bd5998aa60c6ff9328f60c3c14eb6461123146ee
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 16:22:30 +0100

    created a state class to set up the game

--- MonogameShooterMain/States/State.cs
+++ MonogameShooterMain/States/State.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace MonogameShooterMain.States
 {
-    class State
+    public abstract class State
     {
+        protected Game1 _game;
+
+        protected ContentManager _content;
+
+        public State(Game1 g, ContentManager content)
+        {
+            _game = g;
+
+            _content = content;
+        }
+
+        public abstract void LoadContent();
+
+        public abstract void Update(GameTime gt);
+
+        public abstract void PostUpdate(GameTime gt);
+
+        public abstract void Draw(GameTime gt, SpriteBatch sb);
     }
 }
