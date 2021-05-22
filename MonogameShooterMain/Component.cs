1a118cd2f035a0de727452d0476c5746ce36e3cd
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 13:21:45 +0100

    Added Components class with update and draw code

---
+++ MonogameShooterMain/Component.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonogameShooterMain
+{
+    public abstract class Component
+    {
+        public abstract void Draw(GameTime gt, SpriteBatch sb);
+
+        public abstract void Update(GameTime gt);
+    }
+}
