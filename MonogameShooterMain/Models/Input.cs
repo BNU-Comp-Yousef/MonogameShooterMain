5a75ddd9e6076959e3d0b79c3e98b37d562ef251
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 13:42:47 +0100

    Added keyboard inputs for game

--- MonogameShooterMain/Models/Input.cs
+++ MonogameShooterMain/Models/Input.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
 namespace MonogameShooterMain.Models
 {
-    class Input
+    public class Input
     {
+        public Keys Up { get; set; }
+
+        public Keys Down { get; set; }
+
+        public Keys Right { get; set; }
+
+        public Keys Left { get; set; }
+
+        public Keys Shoot { get; set; }
+
     }
 }
