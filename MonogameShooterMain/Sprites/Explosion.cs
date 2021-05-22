35e424c3dc4b6f1a85422fa27b646fd5a2104b9f
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-22 21:45:55 +0100

    Added Explosion sprite code to make explosion animations

--- MonogameShooterMain/Sprites/Explosion.cs
+++ MonogameShooterMain/Sprites/Explosion.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using MonogameShooterMain.Models;
+
 
 namespace MonogameShooterMain.Sprites
 {
-    class Explosion
+    public class Explosion : Sprite
     {
+        private float _timer = 0f;
+
+        public Explosion(Dictionary<string, Animation> animations) : base(animations)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _animationManager.Update(gameTime);
+
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer > _animationManager.CurrentAnimation.FrameCount * _animationManager.CurrentAnimation.FrameSpeed)
+                IsRemoved = true;
+        }
     }
 }
