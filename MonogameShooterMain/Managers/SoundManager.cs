ccb93aa91883c39e185920a134a87b8198abb4a9
CertifiedToast <72758872+CertifiedToast@users.noreply.github.com>
2021-05-27 09:48:29 +0100

    Added sound manager class

--- MonogameShooterMain/Managers/SoundManager.cs
+++ MonogameShooterMain/Managers/SoundManager.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MonogameShooterMain.Managers
 {
-    class SoundManager
+    public class SoundManager
     {
+        private static readonly Dictionary<string, SoundEffect> SoundEffects =
+            new Dictionary<string, SoundEffect>();
+
+        public static void LoadContent(ContentManager content)
+        {
+            SoundEffects.Add("Flame", content.Load<SoundEffect>("Sound/flame"));
+        }
+
+        public static SoundEffect GetSoundEffect(string effect)
+        {
+            return SoundEffects[effect];
+        }
     }
 }
